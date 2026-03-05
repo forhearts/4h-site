b7546a3dd41a38fce169770a7db3e6271c403c4d
fylcr <1435727255@qq.com>
2026-03-05 21:27:49 +0800

    modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   i18n: {
     locales: ["zh-CN", "en"],
     defaultLocale: "zh-CN",
   },
+
+  integrations: [playformCompress()],
 });
