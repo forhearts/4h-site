0280b8e2a4047ada44ef38c44419def180ff6b6b
fylcr <1435727255@qq.com>
2026-03-05 16:20:50 +0800

    modified:   src/assets/app.css 	modified:   src/components/4h-logo.astro 	modified:   src/components/navbar.astro 	new file:   src/i18n/utils.ts 	modified:   src/layouts/layout.astro 	modified:   src/pages/en/index.astro 	modified:   src/pages/index.astro

---
+++ src/i18n/utils.ts
@@ -0,0 +1,32 @@
+﻿export const languages = {
+    "zh-CN": "简体中文",
+    "en": "English",
+};
+
+export const defaultLang = "zh-CN";
+
+export const ui = {
+    "zh-CN": {
+        "nav.logo": "环心",
+    },
+    "en": {
+        "nav.logo": "ForHearts",
+    },
+} as const;
+
+export function getLangFromUrl(url: URL) {
+    const [, lang] = url.pathname.split("/");
+    if (lang in ui) return lang as keyof typeof ui;
+    return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+    return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+        return ui[lang][key] || ui[defaultLang][key];
+    };
+}
+
+export function getLocalizedPath(lang: keyof typeof ui, path: string = "/") {
+    const prefix = lang === defaultLang ? "" : `/${lang}`;
+    return `${prefix}${path.startsWith("/") ? path : "/" + path}`;
+}
